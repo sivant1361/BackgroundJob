d3905609ff4ab2097f354c484881ddd9b60c6357
sivant1361 <55569255+sivant1361@users.noreply.github.com>
2021-02-23 17:47:24 +0530

    empty react app

---
+++ App.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+
+const App= () =>{
+  return (
+    <View style={styles.container}>
+      <Text>Empty project</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container:{
+    padding: 30
+  }
+});
+
+export default App;
