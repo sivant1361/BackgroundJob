805d1521d82841b11b4464974f86dbf99d1d50dd
sivant1361 <55569255+sivant1361@users.noreply.github.com>
2021-03-17 22:43:55 +0530

    testing background jobs

--- background.js
+++ background.js
@@ -1,6 +1,3 @@
-// import 'background.js';
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
 import BackgroundJob from 'react-native-background-job';
 
 const backgroundJob = {
@@ -17,19 +14,3 @@ var backgroundSchedule = {
 BackgroundJob.schedule(backgroundSchedule)
   .then(() => console.log('Success'))
   .catch((err) => console.err(err));
-
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Empty project</Text>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 30,
-  },
-});
-
-export default App;
